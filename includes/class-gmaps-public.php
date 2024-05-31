d619472c0c9d0bae1eb106f756da34121b9639f6
Jarmo Troska <jarmo@troska.ee>
2024-05-31 10:22:38 +0300

    Minify files depending on WP_DEBUG

--- includes/class-gmaps-public.php
+++ includes/class-gmaps-public.php
@@ -33,6 +33,15 @@ class Gmaps_Public {
     private $version;
 
     /**
+     * Whether the site is in debug mode.
+     *
+     * @since    1.1.1
+     * @var      bool    $is_debug    Whether the site is in debug mode.
+     */
+    public $is_debug;
+
+
+    /**
      * Initialize the class and set its properties.
      *
      * @since     1.0.0
@@ -42,8 +51,30 @@ class Gmaps_Public {
     public function __construct($plugin_name, $version) {
         $this->plugin_name = $plugin_name;
         $this->version = $version;
+        $this->is_debug = defined('WP_DEBUG') ? WP_DEBUG : false;
 
         add_action("wp_enqueue_scripts", [$this, "enqueue_scripts"]);
+    }
+
+    /**
+     * Sets the extension to be minified if debugging is not enabled.
+     *
+     * @since     1.1.1
+     * @param     string    $file_ext    Extension of the file.
+     * @return    string    File extension with possible .min prefix.
+     */
+    public function maybe_minify_file_extension($file_ext)
+    {
+
+        if ($this->is_debug) return $file_ext;
+
+        $extensions_to_minify = [".css", ".js"];
+
+        if (in_array($file_ext, $extensions_to_minify)) {
+            return ".min" . $file_ext;
+        }
+
+        return $file_ext;
     }
 
     /**
@@ -53,7 +84,7 @@ class Gmaps_Public {
      */
     public function enqueue_scripts() {
 
-        wp_enqueue_script($this->plugin_name . '-main-scripts', plugin_dir_url( __DIR__ ) . '/public/js/gmaps.js', array(), $this->version, true);
+        wp_enqueue_script($this->plugin_name . '-main-scripts', plugin_dir_url( __DIR__ ) . '/public/js/gmaps' . $this->maybe_minify_file_extension(".js"), array(), $this->version, true);
 
         $google_maps_api_key = get_option("gmaps_api_key");
 
